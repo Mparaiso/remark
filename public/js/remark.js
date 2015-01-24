1da3d9466b65e2ff2f582ec74a2f737ed9acc04d
mparaiso <aikah@free.fr>
2015-01-24 04:11:54 +0100

    Note and Notes

--- public/js/remark.js
+++ public/js/remark.js
@@ -1,34 +1,68 @@
 /*global Parse,angular*/
 "use strict";
-angular.module('remark',['ui.router'],function(Parse,$urlRouterProvider,$stateProvider){
+angular.module('remark', ['ui.router'], function (Parse, $urlRouterProvider, $stateProvider) {
     Parse.initialize("ybjJj7M2jfPIVi6LJGUhjQtjm20IpzQM0nyv7NzK", "pL1iooSxOsrNVCxq7EPfJB8UtiMZvwT5mkOwePJE");
     $urlRouterProvider.otherwise('/notes');
     $stateProvider
-        .state('notes',{
-            url:'/notes',
-            templateUrl:'templates/home.html',
+        .state('notes', {
+            url: '/notes',
+            templateUrl: 'templates/home.html',
+            controller: function () { }
         })
-        .state('note-new',{
-            url:'/note-new',
-            templateUrl:'templates/note-new.html'
+        .state('note-new', {
+            url: '/note/new',
+            templateUrl: 'templates/note-new.html',
+            controller: function ($scope, Note, $q, $location) {
+                $scope.save = function (note) {
+                    var note = new Note({ title: note.title, content: note.content });
+                    $q(function (res, rej) { note.save().then(res, rej) })
+                    .then(function (note) {
+                        $location.path('/note/' + node.id);
+                    });
+                };
+            }
         })
-        .state('dashboard',{
-            url:'/dashboard',
-            templateUrl:'templates/dashboard.html'
+        .state('/note/:id', {
+            templateUrl: 'templates/note-read.html',
+            resolve: {
+                note: function (Note, $q) {
+                    $q(function (res, rej) {
+
+                    })
+                }
+            },
+            controller: function () { }
         })
-        .state('sign-in',{
-            url:'/sign-in',
-            templateUrl:'templates/sign-in.html'
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'templates/dashboard.html'
         })
-        .state('sign-up',{
-            url:'/sign-up',
-            templateUrl:'templates/sign-up.html',
-            controller:function ($scope) {
-                $scope.register=function(){
+        .state('sign-in', {
+            url: '/sign-in',
+            templateUrl: 'templates/sign-in.html'
+        })
+        .state('sign-up', {
+            url: '/sign-up',
+            templateUrl: 'templates/sign-up.html',
+            controller: function ($scope) {
+                $scope.register = function () {
                 };
             }
         });
 })
-.constant('Parse',Parse)
-.constant('VERSION',"0.1")
-.run(function run(){});
+.constant('Parse', Parse)
+.factory('Note', function (Parse) {
+    return Parse.Object.extend('Note', {}, {
+        getById: function (id) {
+            var self = this;
+            return $q(function (res, rej) {
+                (new Parse.Query(self)).get(id).then(res, rej);
+            })
+        }
+    });
+})
+.factory('Notes', function (Parse, Note) {
+    return Parse.Collection.extend({ model: Note });
+})
+.constant('VERSION', "0.1")
+.run(function run() { });
