49d23320dd5ecb521916c76dd1c019327ccd91ce
mparaiso <aikah@free.fr>
2015-01-24 06:06:24 +0100

    dashboard displays list of notes

--- public/js/remark.js
+++ public/js/remark.js
@@ -1,26 +1,23 @@
 /*global Parse,angular*/
-"use strict";
-angular.module('remark', ['ui.router'], function (Parse, $urlRouterProvider, $stateProvider) {
-    Parse.initialize("ybjJj7M2jfPIVi6LJGUhjQtjm20IpzQM0nyv7NzK", "pL1iooSxOsrNVCxq7EPfJB8UtiMZvwT5mkOwePJE");
-    $urlRouterProvider.otherwise('/notes');
-    $stateProvider
-        .state('notes', {
-            url: '/notes',
-            templateUrl: 'templates/home.html',
-            resolve: function (Note) {
-                return Note.query();
-            },
-            controller: function (notes) { $scope.notes = notes; }
+(function () {
+    "use strict";
+    angular.module('remark', ['ui.router'], function (Parse, $urlRouterProvider, $stateProvider) {
+        Parse.initialize("ybjJj7M2jfPIVi6LJGUhjQtjm20IpzQM0nyv7NzK", "pL1iooSxOsrNVCxq7EPfJB8UtiMZvwT5mkOwePJE");
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'templates/home.html'
         })
         .state('note-new', {
-            url: '/note/new',
+            url: '/note-new',
             templateUrl: 'templates/note-new.html',
             controller: function ($scope, Note, $q, $location) {
                 $scope.save = function (note) {
                     var note = new Note({ title: note.title, content: note.content });
                     $q(function (res, rej) { note.save().then(res, rej) })
                     .then(function (note) {
-                        $location.path('/note/' + node.id);
+                        $location.path('/note/' + note.id);
                     });
                 };
             }
@@ -29,17 +26,23 @@ angular.module('remark', ['ui.router'], function (Parse, $urlRouterProvider, $st
             url: '/note/:id',
             templateUrl: 'templates/note-read.html',
             resolve: {
-                note: function (Note, $urlParams, $q) {
-                    return Note.getById($urlParams.id)
+                note: function (Note, $stateParams) {
+                    return Note.getById($stateParams.id);
                 }
             },
-            controller: function (note) {
+            controller: function ($scope, note) {
                 $scope.note = note;
             }
         })
         .state('dashboard', {
             url: '/dashboard',
-            templateUrl: 'templates/dashboard.html'
+            templateUrl: 'templates/dashboard.html',
+            resolve: {
+                notes: function (Note) {
+                    return Note.query();
+                }
+            },
+            controller: function ($scope, notes) { $scope.notes = notes; }
         })
         .state('sign-in', {
             url: '/sign-in',
@@ -53,21 +56,26 @@ angular.module('remark', ['ui.router'], function (Parse, $urlRouterProvider, $st
                 };
             }
         });
-})
+    })
 .constant('Parse', Parse)
-.factory('Note', function (Parse) {
+.factory('Note', function (Parse, $q) {
     return Parse.Object.extend('Note', {}, {
         query: function (limit, skip) {
             var self = this, limit = limit || 100, skip = skip || 0;
             return $q(function (res, rej) {
                 (new Parse.Query(self)).limit(limit).skip(skip).find().then(res, rej);
+            }).then(function (notes) {
+                console.log(notes);
+                return (new Parse.Collection(notes)).toJSON();
             })
         },
         getById: function (id) {
             var self = this;
             return $q(function (res, rej) {
                 (new Parse.Query(self)).get(id).then(res, rej);
-            })
+            }).then(function (note) {
+                return note.toJSON();
+            });
         }
     });
 })
@@ -76,3 +84,4 @@ angular.module('remark', ['ui.router'], function (Parse, $urlRouterProvider, $st
 })
 .constant('VERSION', "0.1")
 .run(function run() { });
+} ());
